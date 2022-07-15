33106a1c330f2c1a7723b5886fceb2b358f28c5a
Kattagarian <malkianju@gmail.com>
2022-07-15 16:04:02 -0300

    scripts nand

---
+++ python-beginner-ms/hdlloop.py/Mux16.py
@@ -0,0 +1,4 @@
+x = int(input())
+
+for i in range(x):
+    print(f"Mux(a=a[{i}], b=b[{i}], sel=sel, out=out[{i}]);")
---
+++ python-beginner-ms/hdlloop.py/or8way.py
@@ -0,0 +1,6 @@
+#Or(a=in[0], b=in[0], out=out[0]);
+
+x = int(input())
+
+for i in range(x):
+    print(f"Or(a=in[{i}], b=in[{i}], out=out{i});")
